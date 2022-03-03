9cd66fc39e19ed9de895b36ef174ca88e9ffb839
Ilya <milyavskyfamily@gmail.com>
2022-03-03 16:35:41 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+//first test
+main.remove()
+//second test and third test
+const newHeader = document.createElement ('h1');
+newHeader.id = ('victory');
+//fourth test
+const championName = "Ilya"
+newHeader.textContent = `${championName} is the champion`
+
